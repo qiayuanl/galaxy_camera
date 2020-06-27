134df4d2b78043979933ddd64235947dba58c5e8
qiayuan <liaoqiayuan@gmail.com>
2020-06-27 21:16:06 +0800

    add galaxy_camera

--- include/galaxy_camera.h
+++ include/galaxy_camera.h
@@ -4,19 +4,29 @@
 
 #ifndef SRC_GALAXY_CAMERA_INCLUDE_GALAXY_CAMERA_H_
 #define SRC_GALAXY_CAMERA_INCLUDE_GALAXY_CAMERA_H_
-#include <opencv/cv.hpp>
 #include "libgxiapi/GxIAPI.h"
-#include"libgxiapi/DxImageProc.h"
+#include "libgxiapi/DxImageProc.h"
 #include <image_transport/image_transport.h>
-
-class Camera {
+namespace galaxy_camera {
+class GalaxyCamera {
  public:
+  GalaxyCamera(image_transport::Publisher
+               &pub, uint32_t height, uint32_t width, uint32_t step,
+               uint32_t offset_x, uint32_t offset_y,
+               const std::string &encoding);
+  ~GalaxyCamera();
+  bool setAutoGain(bool i);
+  bool setAutoBalance(bool i);
+  bool setAutoExposure(bool i);
+ private:
+  GX_DEV_HANDLE dev_handle_{};
+
   static char *img;
-  static bool inited_;
   static image_transport::Publisher pub_;
   static sensor_msgs::Image image;
-  Camera(image_transport::Publisher &pub);
-  static void GX_STDC OnFrameCallbackFun(GX_FRAME_CALLBACK_PARAM *pFrame);
+  static void GX_STDC onFrameCB(GX_FRAME_CALLBACK_PARAM *pFrame);
+
 };
+}
 
 #endif //SRC_GALAXY_CAMERA_INCLUDE_GALAXY_CAMERA_H_
--- src/galaxy_camera.cpp
+++ src/galaxy_camera.cpp
@@ -3,45 +3,110 @@
 //
 
 #include <galaxy_camera.h>
-#include <cv_bridge/cv_bridge.h>
-void Camera::OnFrameCallbackFun(GX_FRAME_CALLBACK_PARAM *pFrame) {
-  {
-    if (pFrame->status == GX_FRAME_STATUS_SUCCESS) {
-      //对图像进行某些操作
-      cv::Mat cv_img;
-      //cv_img.create(pFrame->nHeight, pFrame->nWidth, CV_8UC3);
-      if (!inited_) {
-        img = new char[(pFrame->nHeight) * (pFrame->nWidth) * 3];
-        inited_ = true;
-      }
 
-      DxRaw8toRGB24((void *) pFrame->pImgBuf,
-                    img,
-                    pFrame->nWidth,
-                    pFrame->nHeight,
-                    RAW2RGB_NEIGHBOUR,
-                    BAYERBG,
-                    false);
-      //memcpy(cv_img.data, img, (pFrame->nHeight) * (pFrame->nWidth) * 3);
-      //sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_img).toImageMsg();
-      memcpy((char *) (&image.data[0]), img, 1024 * 1280 * 3);
-      pub_.publish(image);
-      //cout <<  << endl;
-      //cv::imshow("camera1", cv_img);
-      //cv::waitKey(1);
-    }
-  }
-}
-Camera::Camera(image_transport::Publisher &pub) {
+namespace galaxy_camera {
+
+GalaxyCamera::GalaxyCamera(image_transport::Publisher &pub,
+                           uint32_t height, uint32_t width,
+                           uint32_t offset_x, uint32_t offset_y,
+                           uint32_t step,
+                           const std::string &encoding) {
   pub_ = pub;
-  image.height = 1024;
-  image.width = 1280;
-  image.step = 1280 * 3;
-  image.data.resize(1024 * 1280 * 3);
-  image.encoding = "bgr8";
+  image.height = height;
+  image.width = width;
+  image.step = step;
+  image.data.resize(height * step);
+  image.encoding = encoding;
+  img = new char[height * step];
+
+  assert(GXInitLib() == GX_STATUS_SUCCESS); // Initializes the library.
+  uint32_t device_num = 0;
+  GXUpdateDeviceList(&device_num, 1000);
+  assert(device_num == 1); // TODO add multi camera support.
+  // Opens the device.
+  GX_OPEN_PARAM open_param;
+  open_param.accessMode = GX_ACCESS_EXCLUSIVE;
+  open_param.openMode = GX_OPEN_INDEX;
+  open_param.pszContent = (char *) "1";
+  // Get handle
+  assert(GXOpenDevice(&open_param, &dev_handle_) == GX_STATUS_SUCCESS);
+  ROS_INFO("Camera Opened");
+
+  int64_t format = 0;
+  if (encoding == "mono8")
+    format = GX_PIXEL_FORMAT_MONO8;
+  if (encoding == "mono16")
+    format = GX_PIXEL_FORMAT_MONO16;
+  if (encoding == "bgr8")
+    format = GX_PIXEL_FORMAT_BAYER_GB8;
+  if (encoding == "rgb8")
+    format = GX_PIXEL_FORMAT_BAYER_RG8;
+  if (encoding == "bgra8")
+    format = GX_PIXEL_FORMAT_BAYER_BG8;
+  if (format == 0)
+      static_assert(true, "Illegal format");
+
+  assert(GXSetEnum(dev_handle_, GX_ENUM_PIXEL_FORMAT, format) == GX_STATUS_SUCCESS);
+  assert(GXSetInt(dev_handle_, GX_INT_WIDTH, width) == GX_STATUS_SUCCESS);
+  assert(GXSetInt(dev_handle_, GX_INT_HEIGHT, height) == GX_STATUS_SUCCESS);
+  assert(GXSetInt(dev_handle_, GX_INT_OFFSET_X, offset_x) == GX_STATUS_SUCCESS);
+  assert(GXSetInt(dev_handle_, GX_INT_OFFSET_Y, offset_y) == GX_STATUS_SUCCESS);
+
+  GXRegisterCaptureCallback(dev_handle_,
+                            nullptr,
+                            GalaxyCamera::onFrameCB);
+  GXStreamOn(dev_handle_);
+  ROS_INFO("Stream On.");
 }
 
-bool Camera::inited_ = false;
-char *Camera::img;
-sensor_msgs::Image Camera::image;
-image_transport::Publisher Camera::pub_;
+void GalaxyCamera::onFrameCB(GX_FRAME_CALLBACK_PARAM *pFrame) {
+
+  if (pFrame->status == GX_FRAME_STATUS_SUCCESS) {
+
+    DxRaw8toRGB24((void *) pFrame->pImgBuf, img,
+                  pFrame->nWidth, pFrame->nHeight,
+                  RAW2RGB_NEIGHBOUR, BAYERBG, false);
+    memcpy((char *) (&image.data[0]), img, image.step * image.height);
+    pub_.publish(image);
+  }
+
+}
+
+bool GalaxyCamera::setAutoGain(bool i) {
+  GX_STATUS status;
+  if (i)
+    status = GXSetEnum(dev_handle_, GX_ENUM_GAIN_AUTO, GX_GAIN_AUTO_CONTINUOUS);
+  else
+    status = GXSetEnum(dev_handle_, GX_ENUM_GAIN_AUTO, GX_GAIN_AUTO_OFF);
+  return status == GX_STATUS_SUCCESS;
+}
+
+bool GalaxyCamera::setAutoBalance(bool i) {
+  GX_STATUS status;
+  if (i)
+    status = GXSetEnum(dev_handle_, GX_ENUM_GAIN_AUTO, GX_GAIN_AUTO_CONTINUOUS);
+  else
+    status = GXSetEnum(dev_handle_, GX_ENUM_GAIN_AUTO, GX_GAIN_AUTO_OFF);
+  return status == GX_STATUS_SUCCESS;
+}
+
+bool GalaxyCamera::setAutoExposure(bool i) {
+  GX_STATUS status;
+  if (i)
+    status = GXSetEnum(dev_handle_, GX_ENUM_GAIN_AUTO, GX_GAIN_AUTO_CONTINUOUS);
+  else
+    status = GXSetEnum(dev_handle_, GX_ENUM_GAIN_AUTO, GX_GAIN_AUTO_OFF);
+  return status == GX_STATUS_SUCCESS;
+}
+
+GalaxyCamera::~GalaxyCamera() {
+  GXStreamOff(dev_handle_);
+  GXUnregisterCaptureCallback(dev_handle_);
+  GXCloseDevice(dev_handle_);
+  GXCloseLib();
+}
+
+char *GalaxyCamera::img;
+sensor_msgs::Image GalaxyCamera::image;
+image_transport::Publisher GalaxyCamera::pub_;
+}

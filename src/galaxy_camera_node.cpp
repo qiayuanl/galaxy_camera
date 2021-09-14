91c7e99b6b70611c985793077116073dd6bcfc93
Jie j <1346758068@qq.com>
2021-09-14 17:15:17 +0800

    Support to identify multi-device and open camera by sn

--- src/galaxy_camera_node.cpp
+++ src/galaxy_camera_node.cpp
@@ -4,16 +4,15 @@
 #include <galaxy_camera.h>
 #include <nodelet/loader.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "galaxy_camera_node");
 
   nodelet::Loader nodelet;
-  const nodelet::M_string &remap(ros::names::getRemappings());
+  const nodelet::M_string& remap(ros::names::getRemappings());
   nodelet::V_string nargv;
 
-  nodelet.load(ros::this_node::getName(),
-               "galaxy_camera/GalaxyCameraNodelet",
-               remap, nargv);
+  nodelet.load(ros::this_node::getName(), "galaxy_camera/GalaxyCameraNodelet", remap, nargv);
 
   ros::spin();
   return 0;

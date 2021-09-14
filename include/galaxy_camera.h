e7b03866af30fffae8b6ba38556b20e7f61f2da3
Jie j <1346758068@qq.com>
2021-09-14 17:15:17 +0800

    support to identify multi-device and open camera by sn

--- include/galaxy_camera.h
+++ include/galaxy_camera.h
@@ -11,34 +11,36 @@
 #include <dynamic_reconfigure/server.h>
 #include <galaxy_camera/CameraConfig.h>
 #include <camera_info_manager/camera_info_manager.h>
+#include <dynamic_reconfigure/client.h>
+#include <std_msgs/UInt8.h>
 
-namespace galaxy_camera {
-class GalaxyCameraNodelet : public nodelet::Nodelet {
- public:
+namespace galaxy_camera
+{
+class GalaxyCameraNodelet : public nodelet::Nodelet
+{
+public:
   GalaxyCameraNodelet();
   ~GalaxyCameraNodelet() override;
 
   void onInit() override;
-  image_transport::CameraPublisher image_pub_;
   static sensor_msgs::Image image_;
 
- private:
-  void reconfigCB(CameraConfig &config, uint32_t level);
+private:
+  void reconfigCB(CameraConfig& config, uint32_t level);
 
   ros::NodeHandle nh_;
   GX_DEV_HANDLE dev_handle_{};
-  dynamic_reconfigure::Server<CameraConfig> *srv_{};
+  dynamic_reconfigure::Server<CameraConfig>* srv_{};
   int last_channel_ = 0;
 
   boost::shared_ptr<camera_info_manager::CameraInfoManager> info_manager_;
-  std::string camera_name_, camera_info_url_, pixel_format_, frame_id_;
-  int image_width_{}, image_height_{}, image_offset_x_{}, image_offset_y_{};
-  static char *img_;
+  std::string camera_name_, camera_info_url_, pixel_format_, frame_id_, camera_sn_;
+  int image_width_{}, image_height_{}, image_offset_x_{}, image_offset_y_{}, exposure_value_{};
+  static char* img_;
   static image_transport::CameraPublisher pub_;
   static sensor_msgs::CameraInfo info_;
-  static void GX_STDC onFrameCB(GX_FRAME_CALLBACK_PARAM *pFrame);
-
+  static void GX_STDC onFrameCB(GX_FRAME_CALLBACK_PARAM* pFrame);
 };
-}
+}  // namespace galaxy_camera
 
-#endif //SRC_GALAXY_CAMERA_INCLUDE_GALAXY_CAMERA_H_
+#endif  // SRC_GALAXY_CAMERA_INCLUDE_GALAXY_CAMERA_H_
--- src/galaxy_camera.cpp
+++ src/galaxy_camera.cpp
@@ -3,18 +3,23 @@
 //
 #include <pluginlib/class_list_macros.h>
 #include <galaxy_camera.h>
-#include <nodelet/loader.h>
 #include <utility>
+#include <ros/time.h>
 
-namespace galaxy_camera {
+namespace galaxy_camera
+{
 PLUGINLIB_EXPORT_CLASS(galaxy_camera::GalaxyCameraNodelet, nodelet::Nodelet)
-GalaxyCameraNodelet::GalaxyCameraNodelet() {}
+GalaxyCameraNodelet::GalaxyCameraNodelet()
+{
+}
 
-void GalaxyCameraNodelet::onInit() {
+void GalaxyCameraNodelet::onInit()
+{
   nh_ = this->getPrivateNodeHandle();
   image_transport::ImageTransport it(nh_);
   pub_ = it.advertiseCamera("image_raw", 1);
-  nh_.param("camera_frame_id", image_.header.frame_id, std::string("camera"));
+
+  nh_.param("camera_frame_id", image_.header.frame_id, std::string("camera_optical_frame"));
   nh_.param("camera_name", camera_name_, std::string("camera"));
   nh_.param("camera_info_url", camera_info_url_, std::string(""));
   nh_.param("image_width", image_width_, 1280);
@@ -22,13 +27,14 @@ void GalaxyCameraNodelet::onInit() {
   nh_.param("image_offset_x", image_offset_x_, 0);
   nh_.param("image_offset_y", image_offset_y_, 0);
   nh_.param("pixel_format", pixel_format_, std::string("bgr8"));
-  nh_.param("frame_id", frame_id_, std::string("camera"));
+  nh_.param("frame_id", frame_id_, std::string("camera_optical_frame"));
+  nh_.param("camera_sn", camera_sn_, std::string(""));
 
-  info_manager_.reset(new camera_info_manager::CameraInfoManager(
-      nh_, camera_name_, camera_info_url_));
+  info_manager_.reset(new camera_info_manager::CameraInfoManager(nh_, camera_name_, camera_info_url_));
 
   // check for default camera info
-  if (!info_manager_->isCalibrated()) {
+  if (!info_manager_->isCalibrated())
+  {
     info_manager_->setCameraName(camera_name_);
     sensor_msgs::CameraInfo camera_info;
     camera_info.header.frame_id = image_.header.frame_id;
@@ -36,8 +42,7 @@ void GalaxyCameraNodelet::onInit() {
     camera_info.height = image_height_;
     info_manager_->setCameraInfo(camera_info);
   }
-  ROS_INFO("Starting '%s' at %dx%d", camera_name_.c_str(),
-           image_width_, image_height_);
+  ROS_INFO("Starting '%s' at %dx%d", camera_name_.c_str(), image_width_, image_height_);
   info_ = std::move(info_manager_->getCameraInfo());
   info_.header.frame_id = frame_id_;
   image_.header.frame_id = frame_id_;
@@ -48,15 +53,27 @@ void GalaxyCameraNodelet::onInit() {
   image_.encoding = pixel_format_;
   img_ = new char[image_.height * image_.step];
 
-  assert(GXInitLib() == GX_STATUS_SUCCESS); // Initializes the library.
+  assert(GXInitLib() == GX_STATUS_SUCCESS);  // Initializes the library.
   uint32_t device_num = 0;
   GXUpdateDeviceList(&device_num, 1000);
-  assert(device_num == 1); // TODO add multi camera support.
-  // Opens the device.
+  assert(device_num > 0);
+
   GX_OPEN_PARAM open_param;
-  open_param.accessMode = GX_ACCESS_EXCLUSIVE;
-  open_param.openMode = GX_OPEN_INDEX;
-  open_param.pszContent = (char *) "1";
+  // Opens the device.
+  if (device_num > 1)
+  {
+    assert(nh_.hasParam("camera_SN"));
+    open_param.accessMode = GX_ACCESS_EXCLUSIVE;
+    open_param.openMode = GX_OPEN_SN;
+    open_param.pszContent = (char*)&camera_sn_[0];
+  }
+  else
+  {
+    open_param.accessMode = GX_ACCESS_EXCLUSIVE;
+    open_param.openMode = GX_OPEN_INDEX;
+    open_param.pszContent = (char*)"1";
+  }
+
   // Get handle
   assert(GXOpenDevice(&open_param, &dev_handle_) == GX_STATUS_SUCCESS);
   ROS_INFO("Camera Opened");
@@ -73,40 +90,31 @@ void GalaxyCameraNodelet::onInit() {
   if (pixel_format_ == "bgra8")
     format = GX_PIXEL_FORMAT_BAYER_BG8;
   if (format == 0)
-      static_assert(true, "Illegal format");
+    static_assert(true, "Illegal format");
 
-  //assert(GXSetEnum(dev_handle_, GX_ENUM_PIXEL_FORMAT, format) == GX_STATUS_SUCCESS);
-  assert(
-      GXSetInt(dev_handle_, GX_INT_WIDTH, image_width_) == GX_STATUS_SUCCESS);
-  assert(
-      GXSetInt(dev_handle_, GX_INT_HEIGHT, image_height_) == GX_STATUS_SUCCESS);
-  assert(GXSetInt(dev_handle_, GX_INT_OFFSET_X, image_offset_x_)
-             == GX_STATUS_SUCCESS);
-  assert(GXSetInt(dev_handle_, GX_INT_OFFSET_Y, image_offset_y_)
-             == GX_STATUS_SUCCESS);
-
-  GXRegisterCaptureCallback(dev_handle_,
-                            nullptr,
-                            onFrameCB);
+  // assert(GXSetEnum(dev_handle_, GX_ENUM_PIXEL_FORMAT, format) == GX_STATUS_SUCCESS);
+  assert(GXSetInt(dev_handle_, GX_INT_WIDTH, image_width_) == GX_STATUS_SUCCESS);
+  assert(GXSetInt(dev_handle_, GX_INT_HEIGHT, image_height_) == GX_STATUS_SUCCESS);
+  assert(GXSetInt(dev_handle_, GX_INT_OFFSET_X, image_offset_x_) == GX_STATUS_SUCCESS);
+  assert(GXSetInt(dev_handle_, GX_INT_OFFSET_Y, image_offset_y_) == GX_STATUS_SUCCESS);
+  GXRegisterCaptureCallback(dev_handle_, nullptr, onFrameCB);
   GXStreamOn(dev_handle_);
   ROS_INFO("Stream On.");
-  GXSetEnum(dev_handle_, GX_ENUM_BALANCE_WHITE_AUTO,
-            GX_BALANCE_WHITE_AUTO_CONTINUOUS);
+  GXSetEnum(dev_handle_, GX_ENUM_BALANCE_WHITE_AUTO, GX_BALANCE_WHITE_AUTO_CONTINUOUS);
 
-  ros::NodeHandle p_nh(nh_, "camera");
+  ros::NodeHandle p_nh(nh_, "galaxy_camera_dy");
   srv_ = new dynamic_reconfigure::Server<CameraConfig>(p_nh);
-  dynamic_reconfigure::Server<CameraConfig>::CallbackType
-      cb = boost::bind(&GalaxyCameraNodelet::reconfigCB, this, _1, _2);
+  dynamic_reconfigure::Server<CameraConfig>::CallbackType cb =
+      boost::bind(&GalaxyCameraNodelet::reconfigCB, this, _1, _2);
   srv_->setCallback(cb);
 }
 
-void GalaxyCameraNodelet::onFrameCB(GX_FRAME_CALLBACK_PARAM *pFrame) {
-  if (pFrame->status == GX_FRAME_STATUS_SUCCESS) {
-
-    DxRaw8toRGB24((void *) pFrame->pImgBuf, img_,
-                  pFrame->nWidth, pFrame->nHeight,
-                  RAW2RGB_NEIGHBOUR, BAYERBG, false);
-    memcpy((char *) (&image_.data[0]), img_, image_.step * image_.height);
+void GalaxyCameraNodelet::onFrameCB(GX_FRAME_CALLBACK_PARAM* pFrame)
+{
+  if (pFrame->status == GX_FRAME_STATUS_SUCCESS)
+  {
+    DxRaw8toRGB24((void*)pFrame->pImgBuf, img_, pFrame->nWidth, pFrame->nHeight, RAW2RGB_NEIGHBOUR, BAYERBG, false);
+    memcpy((char*)(&image_.data[0]), img_, image_.step * image_.height);
     ros::Time now = ros::Time::now();
     image_.header.stamp = now;
     info_.header.stamp = now;
@@ -114,87 +122,90 @@ void GalaxyCameraNodelet::onFrameCB(GX_FRAME_CALLBACK_PARAM *pFrame) {
   }
 }
 
-void GalaxyCameraNodelet::reconfigCB(CameraConfig &config, uint32_t level) {
-  (void) level;
+void GalaxyCameraNodelet::reconfigCB(CameraConfig& config, uint32_t level)
+{
+  (void)level;
   // Exposure
-  if (config.exposure_auto) {
+  if (config.exposure_auto)
+  {
     double value;
-    GXSetFloat(dev_handle_,
-               GX_FLOAT_AUTO_EXPOSURE_TIME_MAX, config.exposure_max);
-    GXSetFloat(dev_handle_,
-               GX_FLOAT_AUTO_EXPOSURE_TIME_MIN, config.exposure_min);
-    GXSetEnum(dev_handle_,
-              GX_ENUM_EXPOSURE_AUTO, GX_EXPOSURE_AUTO_CONTINUOUS);
+    GXSetFloat(dev_handle_, GX_FLOAT_AUTO_EXPOSURE_TIME_MAX, config.exposure_max);
+    GXSetFloat(dev_handle_, GX_FLOAT_AUTO_EXPOSURE_TIME_MIN, config.exposure_min);
+    GXSetEnum(dev_handle_, GX_ENUM_EXPOSURE_AUTO, GX_EXPOSURE_AUTO_CONTINUOUS);
     GXGetFloat(dev_handle_, GX_FLOAT_EXPOSURE_TIME, &value);
     config.exposure_value = value;
-  } else {
+  }
+  else
+  {
     GXSetEnum(dev_handle_, GX_ENUM_EXPOSURE_AUTO, GX_EXPOSURE_AUTO_OFF);
     GXSetFloat(dev_handle_, GX_FLOAT_EXPOSURE_TIME, config.exposure_value);
   }
 
   // Gain
-  if (config.gain_auto) {
+  if (config.gain_auto)
+  {
     GXSetFloat(dev_handle_, GX_FLOAT_AUTO_GAIN_MIN, config.gain_min);
     GXSetFloat(dev_handle_, GX_FLOAT_AUTO_GAIN_MAX, config.gain_max);
     GXSetEnum(dev_handle_, GX_ENUM_GAIN_AUTO, GX_GAIN_AUTO_CONTINUOUS);
     GXGetFloat(dev_handle_, GX_FLOAT_GAIN, &config.gain_value);
-  } else {
+  }
+  else
+  {
     GXSetEnum(dev_handle_, GX_ENUM_GAIN_AUTO, GX_GAIN_AUTO_OFF);
     GXSetFloat(dev_handle_, GX_FLOAT_GAIN, config.gain_value);
   }
 
   // Black level
-  if (config.black_auto) {
-    GXSetEnum(dev_handle_,
-              GX_ENUM_BLACKLEVEL_AUTO,
-              GX_BLACKLEVEL_AUTO_CONTINUOUS);
+  if (config.black_auto)
+  {
+    GXSetEnum(dev_handle_, GX_ENUM_BLACKLEVEL_AUTO, GX_BLACKLEVEL_AUTO_CONTINUOUS);
     GXGetFloat(dev_handle_, GX_FLOAT_BLACKLEVEL, &config.black_value);
-  } else {
+  }
+  else
+  {
     GXSetEnum(dev_handle_, GX_ENUM_BLACKLEVEL_AUTO, GX_BLACKLEVEL_AUTO_OFF);
     GXSetFloat(dev_handle_, GX_FLOAT_BLACKLEVEL, config.black_value);
   }
   // Balance White
-  switch (config.white_selector) {
+  switch (config.white_selector)
+  {
     case 0:
-      GXSetEnum(dev_handle_,
-                GX_ENUM_BALANCE_RATIO_SELECTOR, GX_BALANCE_RATIO_SELECTOR_RED);
+      GXSetEnum(dev_handle_, GX_ENUM_BALANCE_RATIO_SELECTOR, GX_BALANCE_RATIO_SELECTOR_RED);
       break;
     case 1:
-      GXSetEnum(dev_handle_,
-                GX_ENUM_BALANCE_RATIO_SELECTOR,
-                GX_BALANCE_RATIO_SELECTOR_GREEN);
+      GXSetEnum(dev_handle_, GX_ENUM_BALANCE_RATIO_SELECTOR, GX_BALANCE_RATIO_SELECTOR_GREEN);
       break;
     case 2:
-      GXSetEnum(dev_handle_,
-                GX_ENUM_BALANCE_RATIO_SELECTOR, GX_BALANCE_RATIO_SELECTOR_BLUE);
+      GXSetEnum(dev_handle_, GX_ENUM_BALANCE_RATIO_SELECTOR, GX_BALANCE_RATIO_SELECTOR_BLUE);
       break;
   }
-  if (last_channel_ != config.white_selector) {
+  if (last_channel_ != config.white_selector)
+  {
     GXGetFloat(dev_handle_, GX_FLOAT_BALANCE_RATIO, &config.white_value);
     last_channel_ = config.white_selector;
   }
-  if (config.white_auto) {
-    GXSetEnum(dev_handle_,
-              GX_ENUM_BALANCE_WHITE_AUTO,
-              GX_BALANCE_WHITE_AUTO_CONTINUOUS);
+  if (config.white_auto)
+  {
+    GXSetEnum(dev_handle_, GX_ENUM_BALANCE_WHITE_AUTO, GX_BALANCE_WHITE_AUTO_CONTINUOUS);
     GXGetFloat(dev_handle_, GX_FLOAT_BALANCE_RATIO, &config.white_value);
-  } else {
-    GXSetEnum(dev_handle_,
-              GX_ENUM_BALANCE_WHITE_AUTO,
-              GX_BALANCE_WHITE_AUTO_OFF);
+  }
+  else
+  {
+    GXSetEnum(dev_handle_, GX_ENUM_BALANCE_WHITE_AUTO, GX_BALANCE_WHITE_AUTO_OFF);
     GXSetFloat(dev_handle_, GX_FLOAT_BALANCE_RATIO, config.white_value);
   }
 }
 
-GalaxyCameraNodelet::~GalaxyCameraNodelet() {
+GalaxyCameraNodelet::~GalaxyCameraNodelet()
+{
   GXStreamOff(dev_handle_);
   GXUnregisterCaptureCallback(dev_handle_);
   GXCloseDevice(dev_handle_);
   GXCloseLib();
 }
 
-char *GalaxyCameraNodelet::img_;
+char* GalaxyCameraNodelet::img_;
 sensor_msgs::Image GalaxyCameraNodelet::image_;
 image_transport::CameraPublisher GalaxyCameraNodelet::pub_;
 sensor_msgs::CameraInfo GalaxyCameraNodelet::info_;
-}
+}  // namespace galaxy_camera
